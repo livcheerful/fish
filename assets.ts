2d3c878f7ef600f33aa80c3ee5cd7afcd23b22b4
Vivian Li <vivl@microsoft.com>
2020-12-10 13:17:41 -0800

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,11 @@
+namespace fishSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite4 = image.ofBuffer(hex``);
+
+}
